19a64bcd8812aaf432e82c7664cb2e441c1e43f2
Nazar Vinnichuk <nazar@vinnich.uk>
2022-08-28 16:42:00 +0300

    Generate a Dart CLI project.

---
+++ bin/mpassless.dart
@@ -0,0 +1,5 @@
+import 'package:mpassless/mpassless.dart' as mpassless;
+
+void main(List<String> arguments) {
+  print('Hello world: ${mpassless.calculate()}!');
+}
---
+++ test/mpassless_test.dart
@@ -0,0 +1,8 @@
+import 'package:mpassless/mpassless.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
